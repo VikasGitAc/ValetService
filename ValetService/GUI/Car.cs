674238978e0da3691160863b7f0e5520996e0c7d
Vikas <mailatvikasmodi@gmail.com>
2022-11-14 13:26:40 -0500

    Add project files.

---
+++ ValetService/GUI/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValetService.GUI
+{
+    public class Car { 
+        public int CheckIn_Id { get; set; }
+
+        public int Slip_Id { get; set; }
+        public int Room_No { get; set; }
+        public string Model { get; set; }
+        public string PrakingLot { get; set; }
+        public string Parking_Spot { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public Boolean Car_In { get; set; }
+        public Boolean Car_Out { get; set; }
+
+    }
+}
